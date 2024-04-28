09cd37f6b8a26d3979c8d53fb3084594262d8625
Lucas Gomes <lucaass1997rp@gmail.com>
2024-04-28 16:40:43 -0300

    adicionando animação

--- app.js
+++ app.js
@@ -1,54 +1,84 @@
+function submitEmail() {
+    const formElement = document.getElementById('contactForm');
 
-const formElement = document.getElementById('contactForm');
+    const handleFormSubmit = (event) => {
+        event.preventDefault(); 
 
-const handleFormSubmit = (event) => {
-    event.preventDefault(); 
+        const data = new FormData(formElement);
 
-    const data = new FormData(formElement);
+        fetch(formElement.action, {
+            method: formElement.method,
+            body: data,
+            headers: {
+                'Accept': 'application/json'
+            },
+        }).then(response => {
+            if (response.ok) {
+                
+                formElement.reset(); 
+                alert("Formulário enviado com sucesso!");
+            } else {
+                response.json().then(data => {
+                    if (data.errors) {
+                        alert(data.errors.map(e => e.message).join(", "));
+                    } else {
+                        alert("Oops! Não foi possível enviar o formulário.");
+                    }
+                });
+            }
+        }).catch(error => {
+            alert("Erro ao enviar o formulário: " + error);
+        });
+    };
 
-    fetch(formElement.action, {
-        method: formElement.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        },
-    }).then(response => {
-        if (response.ok) {
-            
-            formElement.reset(); 
-            alert("Formulário enviado com sucesso!");
-        } else {
-            response.json().then(data => {
-                if (data.errors) {
-                    alert(data.errors.map(e => e.message).join(", "));
-                } else {
-                    alert("Oops! Não foi possível enviar o formulário.");
-                }
-            });
-        }
-    }).catch(error => {
-        alert("Erro ao enviar o formulário: " + error);
+    formElement.addEventListener('submit', handleFormSubmit);
+}
+submitEmail()
+
+function menuHamburgue() {
+    const hamburguer = document.querySelector(".hamburguer");
+    const nav = document.querySelector(".nav");
+    const links = document.querySelectorAll(".nav a");
+
+    function toggleNav() {
+        nav.classList.toggle("active");
+    }
+
+    function closeNav() {
+        nav.classList.remove("active");
+    }
+
+    hamburguer.addEventListener("click", toggleNav);
+
+    links.forEach(link => {
+        link.addEventListener("click", closeNav);
     });
-};
-
-formElement.addEventListener('submit', handleFormSubmit);
-
-const hamburguer = document.querySelector(".hamburguer");
-const nav = document.querySelector(".nav");
-const links = document.querySelectorAll(".nav a");
-
-function toggleNav() {
-    nav.classList.toggle("active");
 }
+menuHamburgue()
 
-function closeNav() {
-    nav.classList.remove("active");
+function scrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollToSection(event) {
+        event.preventDefault()
+        const href = event.currentTarget.getAttribute('href')
+        const section = document.querySelector(href)
+        const topo = section.offsetTop
+
+        window.scrollTo({
+            top: topo,
+            behavior: 'smooth',
+        })
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection)
+    })    
 }
+scrollSuave()
 
-hamburguer.addEventListener("click", toggleNav);
-
-links.forEach(link => {
-    link.addEventListener("click", closeNav);
+document.addEventListener("DOMContentLoaded", function() {
+    new WOW().init();
 });
 
 
