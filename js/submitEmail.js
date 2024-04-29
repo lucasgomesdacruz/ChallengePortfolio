9caafb9902f13bddc8fa27ba00f0acbac0f31585
Lucas Gomes <lucaass1997rp@gmail.com>
2024-04-29 15:20:04 -0300

    adicionando module para js

--- js/submitEmail.js
+++ js/submitEmail.js
@@ -1,4 +1,4 @@
-function submitEmail() {
+export default function submitEmail() {
     const formElement = document.getElementById('contactForm');
 
     const handleFormSubmit = (event) => {
@@ -33,49 +33,11 @@ function submitEmail() {
 
     formElement.addEventListener('submit', handleFormSubmit);
 }
-submitEmail()
 
-function menuHamburgue() {
-    const hamburguer = document.querySelector(".hamburguer");
-    const nav = document.querySelector(".nav");
-    const links = document.querySelectorAll(".nav a");
 
-    function toggleNav() {
-        nav.classList.toggle("active");
-    }
 
-    function closeNav() {
-        nav.classList.remove("active");
-    }
 
-    hamburguer.addEventListener("click", toggleNav);
 
-    links.forEach(link => {
-        link.addEventListener("click", closeNav);
-    });
-}
-menuHamburgue()
-
-function scrollSuave() {
-    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
-
-    function scrollToSection(event) {
-        event.preventDefault()
-        const href = event.currentTarget.getAttribute('href')
-        const section = document.querySelector(href)
-        const topo = section.offsetTop
-
-        window.scrollTo({
-            top: topo,
-            behavior: 'smooth',
-        })
-    }
-
-    linksInternos.forEach((link) => {
-        link.addEventListener('click', scrollToSection)
-    })    
-}
-scrollSuave()
 
 
 
---
+++ main.js
@@ -0,0 +1,9 @@
+import menuHamburgue from "./js/menu.js";
+import scrollSuave from "./js/scrollSuave.js"
+import submitEmail from "./js/submitEmail.js";
+
+new WOW().init();
+    
+menuHamburgue()
+scrollSuave()
+submitEmail()
