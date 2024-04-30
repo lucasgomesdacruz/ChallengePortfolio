1820edeaba9e9e5f4c2a3958a1bf035f7f60bba7
Lucas Gomes <lucaass1997rp@gmail.com>
2024-04-30 17:06:09 -0300

    validação de formulario

---
+++ js/validaForm.js
@@ -0,0 +1,16 @@
+export default function validaForm() {
+    const form = document.getElementById('contactForm')
+    function handleChange(event) {
+        const target = event.target
+
+        if(!target.checkValidity()) {
+            target.classList.add('invalido')
+            target.nextElementSibling.innerText = target.validationMessage;
+        } else {
+            target.classList.remove("invalido")
+            target.nextElementSibling.innerText = "";
+            
+        }
+    }
+    form.addEventListener('change', handleChange)
+}
--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import menuHamburgue from "./js/menu.js";
 import scrollSuave from "./js/scrollSuave.js"
 import submitEmail from "./js/submitEmail.js";
+import validaForm from "./js/validaForm.js";
 
 new WOW().init();
     
 menuHamburgue()
 scrollSuave()
 submitEmail()
+validaForm()
